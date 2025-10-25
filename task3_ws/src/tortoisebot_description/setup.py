b037f5d3c12b4b888b259ef598fd12ddb6fccfeb
Hemand Sunny <65974687+hemandsunny@users.noreply.github.com>
2025-10-25 16:43:32 +0530

    Task3 files iniital file upload
    task3 initial file upload.

---
+++ task3_ws/src/tortoisebot_description/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'tortoisebot_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share/',package_name,'config'),glob('config/*')),
+        (os.path.join('share/',package_name,'launch'),glob('launch/*')),
+        (os.path.join('share/',package_name,'models'),glob('models/*')),
+
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='arz-1013',
+    maintainer_email='arz-1013@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
